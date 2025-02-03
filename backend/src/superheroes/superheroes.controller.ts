52a9614a378789ad3967730aa675a40437e623dd
sergei <sergeihuber0724@gmail.com>
2025-02-03 15:41:05 +0200

    feat: add superheroes controller with POST and GET endpoints

---
+++ backend/src/superheroes/superheroes.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { SuperheroesService } from './superheroes.service';
+import { CreateSuperheroDto } from './dto/create-superhero.dto';
+
+@Controller('superheroes')
+export class SuperheroesController {
+  constructor(private readonly superheroesService: SuperheroesService) {}
+
+  @Post()
+  create(@Body() createSuperheroDto: CreateSuperheroDto) {
+    return this.superheroesService.create(createSuperheroDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.superheroesService.findAll();
+  }
+}
